3967efdb9b67aa07f4168f7358503a94eb1c4444
Vincent Velthuizen <mail@vincentvelthuizen.com>
2021-04-22 17:54:30 +0200

    changes from the first half of the session

--- sessions/week_2/variables.py
+++ sessions/week_2/variables.py
@@ -1,11 +1,4 @@
-# get information
-this_is_a_variable = input("Please give me a number ")
-print(this_is_a_variable)
-this_is_a_variable = float(this_is_a_variable)
-print(this_is_a_variable)
+this_is_a_variable = int(input("What is the hour? "))
 
-# do calculation
-calculated_answer = this_is_a_variable + 2
-
-# give output
-print(calculated_answer)
+this_is_a_different_variable = "This is a different String"
+print(this_is_a_variable, this_is_a_different_variable, this_is_a_variable)
